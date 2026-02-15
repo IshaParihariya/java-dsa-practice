b6a89ee9df56b75cba1c56f1c032703f113e3fe5
IshaParihariya <ishh1036@gmail.com>
2026-02-15 20:19:28 +0530

    corrected the code of the insert at Kth position

--- src/linkedlist/operations/insertion/InsertionAtKthPosition.java
+++ src/linkedlist/operations/insertion/InsertionAtKthPosition.java
@@ -1,94 +1,78 @@
-/*
-
-// AN ISSUE IN A PART
-
-insertion at Kth position
-*/
 package linkedlist.operations.insertion;
 
-class InsertionAtKthPositionHelper
-{
-    public Node insert(Node head,int position,int value)
-    {
-        // if the head == null i.e., LL 's length == 0
-        //then dont 
-        // but if position ==1 then just insert at the head
-        if(head==null && position==1)
-        {
-           /*
-            
-            // cannot do this because if head== null so no node exists 
-            // NEED TO CREATE A NEW ONE!**
-            head.data=value;
-            head.next=null;
-            return head;
-*/
-            return new Node(value,null);
+class Node {
+    int data;
+    Node next;
+    Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class InsertionAtKthPositionHelper {
+    public Node insert(Node head, int position, int value) {
+
+        // Case 1: empty list
+        if(head == null) {
+            if(position == 1) return new Node(value, null);
+            else {
+                System.out.println("Invalid position");
+                return null;
+            }
         }
-          else if(head==null)
-            return null;
-        
-        // insertion new Node
-        Node insert=new Node(value,null);// just for initiasation
-        
-        // for insertion at postion 1 or head
-        if(position==1)
-        {
-            insert.next=head;
+
+        Node insert = new Node(value, null);
+
+        // Case 2: insert at head
+        if(position == 1) {
+            insert.next = head;
             return insert;
         }
-        
-        // for LL's length == 1
-        
-        
-        // for LL 's length > 1
-        int count=0;
-        
-        Node del=head;
-        Node temp=head;
-        while(temp!=null)
-        {
+
+        // Case 3: insert in middle or end
+        Node del = null;
+        Node temp = head;
+        int count = 1;
+
+        while(temp != null) {
+            if(count == position) {
+                // insert between del and temp
+                del.next = insert;
+                insert.next = temp;
+                return head;
+            }
+            del = temp;
+            temp = temp.next;
             count++;
-            if(count==position)
-            {
-               del.next=insert;
-               insert.next=temp;
-               return head;
-            }
-            
-            // NOTE : HERE IN THE HANDLING OF THIS THERES AN ISSUE SO HANDLE IT
-            // if insertion need to be done at length + 1
-            if(count<position)
-            {
-                temp.next=insert;
-                insert.next=null;
-            }
-            del=temp;
-            temp=temp.next;
         }
+
+        // Case 4: insert at end (position = length + 1)
+        if(count == position) {
+            del.next = insert;
+        } else {
+            System.out.println("Invalid position");
+        }
+
         return head;
     }
 }
-public class InsertionAtKthPosition
-{
-    public static void main(String[] args)
-    {
-        InsertionAtKthPositionHelper i=new InsertionAtKthPositionHelper();
-        
-        Node tail=new Node(4,null);//tail
-        Node n3=new Node(3,tail);
-        Node n2=new Node(2,n3);
-        Node head=new Node(1,n2);//head
-        
-        head=i.insert(head, 2,0); // insert 0 at 2nd position
-        
-        Node temp=head;
-        
-        while(temp!=null)
-        {
-             System.out.print(temp.data+" -> ");
-            temp=temp.next;
+
+public class InsertionAtKthPosition {
+    public static void main(String[] args) {
+        InsertionAtKthPositionHelper helper = new InsertionAtKthPositionHelper();
+
+        Node tail = new Node(4, null);
+        Node n3 = new Node(3, tail);
+        Node n2 = new Node(2, n3);
+        Node head = new Node(1, n2);
+
+        head = helper.insert(head, 2, 0); // insert 0 at 2nd position
+
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
         }
-          System.out.println("null");
+        System.out.println("null");
     }
 }
