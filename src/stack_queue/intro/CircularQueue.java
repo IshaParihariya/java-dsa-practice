0af07344f677a46cce52cbee9b489fae7561e053
IshaParihariya <ishh1036@gmail.com>
2026-03-25 13:35:24 +0530

    Implemented linear queue using array with proper overflow and underflow handling, including front, rear, and currentSize tracking

---
+++ src/stack_queue/intro/CircularQueue.java
@@ -0,0 +1,11 @@
+// circular queue
+
+package stack_queue.intro;
+
+public class CircularQueue
+{
+   public static void main(String[] args)
+   {
+       
+   }
+}
--- src/stack_queue/intro/ImplementationUsingArray.java
+++ src/stack_queue/intro/ImplementationUsingArray.java
@@ -1,5 +1,6 @@
 /*
 
+// IN QUEUE :
 1. front
 Points to the first element
 Used for removing (poll)
@@ -17,8 +18,6 @@ class StackUsingArray
      int[] stack=new int[10]; // size fixed so static
         
         int top=-1; // top
-        
-        int size=0; // size 
               
         int peek;   // peek
         
@@ -60,134 +59,72 @@ class StackUsingArray
                 }      
             }
         }
+
 }
 
-// for Circular Queue => when reaches end it will start from start again
+
+// This is a linear queue. One limitation is that once the rear
+// reaches the end, we cannot reuse empty space. To optimize this, we can use a circular queue
+// Linear Queue
 class QueueUsingArray
 {
     // offer or add
-    int queue[] =new int[10];
-    
-    int front=-1; // tfront
+    int queue[] =new int[10]; // full size = 10 
+    int size=queue.length;
+    int front=-1; // front
     
     int rear=-1; // rear
     
+    // current size => till where the elements are stored
+    int currentSize=0;
+    
     // offer or add
+    // NOTE : we can also use currentSize comparision with size rather than these length and all conditions
     public void offer(int number)
     {
         // over flow
-        if(front==0&&rear==queue.length-1||front==rear+1)
+        if(rear==queue.length-1)
         {
-            System.out.println("queue overflow");
+            System.out.println("overflow queue");
             return;
         }
         else
         {
-            if(rear==queue.length-1)
-            {
-                 queue[rear]=number;  
-                 rear=-1; // start from the start 
-                 // break here??
-            }
-            if(front==-1)
-            {
+        if(front==-1)
+        {
             front++;
-            }
-            rear++;
-            queue[rear]=number; //this will be the peek 
+        }
+        rear++;
+        queue[rear]=number;
+        currentSize++;   
         }
     }
-    
-    // poll or remove
     public void poll()
     {
-       //under flow
-        if(rear==-1&&front==-1 ||front>rear)
-        { 
+        // underflow
+        if(front==-1&&rear==-1||front>rear)
+        {
             System.out.println("queue underflow");
             return;
         }
         else
         {
-            if(front==queue.length-1)
+            if(front==0&&rear==0)
             {
-                 
-                 front=-1; // start from the start 
-                 // break here??
+                front=-1;
+                rear=-1;
+                currentSize--;
             }
-            
-            front++;
+            else
+            {
+                front++;
+                currentSize--;
+            }
         }
+       
     }
 }
-/*
-GIVEN BY CHATGPT :
 
-class QueueUsingArray
-{
-    int[] queue = new int[10];
-    int front = -1;
-    int rear = -1;
-
-    // OFFER (enqueue)
-    public void offer(int value)
-    {
-        // FULL condition
-        if ((rear + 1) % queue.length == front)
-        {
-            System.out.println("Queue Overflow");
-            return;
-        }
-
-        // First element
-        if (front == -1)
-        {
-            front = 0;
-        }
-
-        rear = (rear + 1) % queue.length;
-        queue[rear] = value;
-    }
-
-    // POLL (dequeue)
-    public int poll()
-    {
-        // EMPTY condition
-        if (front == -1)
-        {
-            System.out.println("Queue Underflow");
-            return -1;
-        }
-
-        int removed = queue[front];
-
-        // Last element removed
-        if (front == rear)
-        {
-            front = -1;
-            rear = -1;
-        }
-        else
-        {
-            front = (front + 1) % queue.length;
-        }
-
-        return removed;
-    }
-
-    // PEEK
-    public int peek()
-    {
-        if (front == -1)
-        {
-            System.out.println("Queue is empty");
-            return -1;
-        }
-
-        return queue[front];
-    }
-}
-*/
 public class ImplementationUsingArray 
 {
     public static void main(String[] args)
@@ -202,7 +139,16 @@ public class ImplementationUsingArray
         s.push(5);
         
         s.pop();
+
         
         // for Queue
+        QueueUsingArray q=new QueueUsingArray();
+        q.offer(1);
+         q.offer(2);
+          q.offer(3);
+           q.offer(4);
+            q.offer(5);
+            
+            q.poll();     
     }
 }
