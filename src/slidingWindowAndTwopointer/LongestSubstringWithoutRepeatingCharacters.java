9149fbf69e1f8bd6439a59210914b595fd58f496
IshaParihariya <ishh1036@gmail.com>
2026-05-20 18:56:35 +0530

    Solve longest substring without repeating characters using sliding window

--- src/slidingWindowAndTwopointer/LongestSubstringWithoutRepeatingCharacters.java
+++ src/slidingWindowAndTwopointer/LongestSubstringWithoutRepeatingCharacters.java
@@ -33,7 +33,11 @@ for(...)
 package slidingWindowAndTwopointer;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
+
+// with loops
+// can be solved with hash as well with O(N^2)
 class  LongestSubstringWithoutRepeatingCharacter
 {
     // method
@@ -92,13 +96,81 @@ class  LongestSubstringWithoutRepeatingCharacter
     }
 }
 
+
+
+// with sliding window and two pointer i.e., 'l' and 'r'
+class  LongestSubstringWithoutRepeatingCharacterWithSWTPM
+{
+    // method
+    public Integer  longestSubstringWithoutRepeatingCharacters(String str)
+    {
+        
+        // hash map : it stores data in key-value pair
+        HashMap<Character,Integer> map = new HashMap<>();
+        // pointers
+        int l=0;
+        int r=0;
+        // length and max length 
+        int length=0;
+        int maxlength=0;
+        // for loop 
+        for(int i=0;i<str.length();i++)
+        {
+            // if the char is already in the hash map
+            if(map.containsKey(str.charAt(i)))
+            {
+                //finding the index using the character
+                Character ch=str.charAt(i);
+                
+                // increment in l 
+              //also this ensures left pointer never goes backward
+              l=Math.max(l,  map.get(ch) + 1);
+                
+                // replace the old index with the new one
+                map.replace(ch, i);
+                
+                // increment in r
+                r++;
+                
+            }
+            // if the char isnt in the
+            else
+            {
+                //store in the hashmap
+                map.put( str.charAt(i),i);
+                
+                // length = r-l+1
+                length=r-l+1;
+                
+                // max length
+                if(length>maxlength)
+                {
+                    maxlength=length;
+                }
+                //increment in r
+                r++;
+            
+            }
+            
+        }
+        
+        //returning the max length 
+        
+        return maxlength;
+    }
+}
 public class LongestSubstringWithoutRepeatingCharacters
 {
      public static void main(String[] args)
      {
-         LongestSubstringWithoutRepeatingCharacter obj =new LongestSubstringWithoutRepeatingCharacter();
-         obj.longestSubstringWithoutRepeatingCharacters("cadbzabcd");
+        // LongestSubstringWithoutRepeatingCharacter obj =new LongestSubstringWithoutRepeatingCharacter();
+         //obj.longestSubstringWithoutRepeatingCharacters("cadbzabcd");
+        
          
+         LongestSubstringWithoutRepeatingCharacterWithSWTPM obj=new LongestSubstringWithoutRepeatingCharacterWithSWTPM();
+         Integer maxlength=obj.longestSubstringWithoutRepeatingCharacters("cadbzabcd");
+         
+         System.out.println(maxlength);
          
      }
 }
